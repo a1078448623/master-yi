c6f721a36a89bea30453ce6e183be023618c41ee
wsn <1078448623@qq.com>
2021-07-06 12:55:12 +0800

    swagger与mybatis正常使用

--- mall/src/main/java/com/wuyan/masteryi/mall/MallApplication.java
+++ mall/src/main/java/com/wuyan/masteryi/mall/MallApplication.java
@@ -3,9 +3,12 @@ package com.wuyan.masteryi.mall;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import springfox.documentation.oas.annotations.EnableOpenApi;
+@EnableOpenApi
+//@MapperScan("com.wuyan.masteryi.mall.mapper")
 @SpringBootApplication
-@MapperScan("com.wuyan.masteryi.mall.mapper")
+
+
 public class MallApplication {
 
     public static void main(String[] args) {
